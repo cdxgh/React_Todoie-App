d1f70d344268173b2e04f2f8b477dab8e8463c27
cdxgh <96329368+cdxgh@users.noreply.github.com>
2023-08-15 20:24:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React_Todoie-App
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-croee4)
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () => {
+  return <p>Todoie App</p>
+};
+export default App;
